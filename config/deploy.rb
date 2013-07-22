7f160f5ef2c23d4d627184108fd9303cf092f89b
Jan Dudulski <jan@dudulski.pl>
2013-07-22 15:59:31 +0200

    Use plain old html

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-require 'mina/bundler'
-require 'mina/rails'
+# require 'mina/bundler'
+# require 'mina/rails'
 require 'mina/git'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (http://rvm.io)
@@ -10,7 +10,7 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, 'cwaniak.eu'
+set :domain, 'cwaniak'
 set :deploy_to, '/var/www/cwaniak.eu'
 set :repository, 'git://github.com/jandudulski/cwaniak.eu.git'
 set :branch, 'master'
@@ -22,7 +22,7 @@ set :shared_paths, ['log', 'tmp']
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
-set :user, 'cwaniak'
+set :user, 'deploy'
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
@@ -56,10 +56,6 @@ task :deploy => :environment do
     # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-
-    to :launch do
-      queue 'touch tmp/restart.txt'
-    end
   end
 end
 
